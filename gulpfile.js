dc6e0675e299309455d1e16f0e41cd1f8340c3ec
andieelmes <andie.elmes@gmail.com>
2018-10-10 20:56:22 +0500

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ var gulp = require('gulp'),
   };
 
 
-  gulp.task('html:build', function () {
+  gulp.task('html:build', function (done) {
     gulp.src(path.src.html)
       .pipe(cache('jading...'))
       .pipe(jade({pretty: true}))
@@ -59,9 +59,10 @@ var gulp = require('gulp'),
       }))
       .pipe(gulp.dest(path.build.html))
       .pipe(reload({stream: true}));
+    done()
   });
 
-  gulp.task('js:build', function () {
+  gulp.task('js:build', function (done) {
     browserify('src/js/main.js')
       .transform(babelify.configure({
         plugins: ["transform-runtime"],
@@ -76,46 +77,41 @@ var gulp = require('gulp'),
       .pipe(source('main.js'))
       .pipe(gulp.dest('build/js/'))
       .pipe(reload({stream: true}))
+    done()
   });
 
 
-  gulp.task('style:build', function () {
+  gulp.task('style:build', function (done) {
     gulp.src(path.src.style)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sass({
-          includePaths: require('node-normalize-scss').includePaths
-        }))
-        .pipe(prefixer({
-            browsers: ['last 4 versions','> 1%','Android 4.4','ios_saf >=7']
-        }))
-        .pipe(gulpif(argv.prod, cleancss()))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(path.build.css))
-        .pipe(reload({stream: true}));
+      .pipe(sourcemaps.init())
+      .pipe(sass({
+        includePaths: require('node-normalize-scss').includePaths
+      }).on('error', sass.logError))
+      .pipe(prefixer({
+          browsers: ['last 4 versions','> 1%','Android 4.4','ios_saf >=7']
+      }))
+      .pipe(gulpif(argv.prod, cleancss()))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(path.build.css))
+      .pipe(reload({stream: true}));
+    done()
   });
 
-  gulp.task('build', [
+  gulp.task('build', gulp.series(
     'html:build',
     'js:build',
     'style:build'
-  ]);
+  ));
 
   gulp.task('watch', function () {
-    watch([path.watch.html], function (event, cb) {
-      gulp.start('html:build');
-    });
-    watch([path.watch.style], function (event, cb) {
-      gulp.start('style:build');
-    });
-    watch([path.watch.js], function (event, cb) {
-      gulp.start('js:build');
-    });
-
+    gulp.watch([path.watch.html], gulp.series("html:build"));
+    gulp.watch([path.watch.style], gulp.series("style:build"));
+    gulp.watch([path.watch.js], gulp.series("js:build"));
   });
 
-  gulp.task('webserver', function () {
+  gulp.task('webserver', function (done) {
     browserSync(config);
+    done()
   });
 
-  gulp.task('default', ['build', 'webserver', 'watch']);
+  gulp.task('default', gulp.series('build', 'webserver', 'watch'));
